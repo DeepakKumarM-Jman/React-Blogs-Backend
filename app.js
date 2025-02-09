f1b5b969d644a22a7e004ee9a1a5058727e96520
Deepak <deepakmuthu1122@gmail.com>
2025-02-09 20:37:20 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import { PORT } from "./config/env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Blogs API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
